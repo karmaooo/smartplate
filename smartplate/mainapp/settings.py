12e547cde479105eb84a593a920d50e7a6f5ddfb
KARMA YANGZOM <12200052.gcit@rub.edu.bt>
2023-06-08 13:38:31 +0600

    f

--- smartplate/mainapp/settings.py
+++ smartplate/mainapp/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ztgworv%36wj^blkq*=z9r733ihbgh_ln&qp0ah9%apb9d+smh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+CSRF_USE_SESSIONS = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -123,9 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build","static" )
+
+# STATICFILES_FINDERS = [
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
